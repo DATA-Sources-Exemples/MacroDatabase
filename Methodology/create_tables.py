e9aa16f722645a404645ea3f5741efb33b6f1686
Jeroen Bouma <jer.bouma@gmail.com>
2022-03-24 10:33:19 +0100

    Add in the automatic filling of README files

--- Methodology/create_tables.py
+++ Methodology/create_tables.py
@@ -1,6 +1,7 @@
 import os
 from tabulate import tabulate
 import pandas as pd
+import json
 
 try:
     path_str = "Database"
@@ -9,6 +10,7 @@ except FileNotFoundError:
     path_str = "../Database"
 
 
+# Create table overview (categories, sub-categories and files)
 table_structure = {}
 
 for top_level in os.listdir(path_str):
@@ -31,7 +33,6 @@ for top_level in os.listdir(path_str):
 
         table_structure[top_level][secondary_level] = amount_of_files
 
-
 reform = {(outerKey, innerKey): values for outerKey, innerDict in table_structure.items()
           for innerKey, values in innerDict.items()}
 df = pd.DataFrame.from_dict(reform, orient='index')
@@ -47,6 +48,61 @@ for irow, (idx, row) in enumerate(df.iterrows()):
 
 df_print[0] = df_print[0].apply(lambda x: "{:,}".format(x).rjust(15))
 
-print(tabulate(
+overview = tabulate(
     df_print, tablefmt="github", headers=['Category', "Sub-Category", "Files"],
-    colalign=("left", "left", "right"), showindex=False))
+    colalign=("left", "left", "right"), showindex=False)
+
+# Create README files for one layer above the folders containing json files
+for directory_path, directory_names, _ in os.walk(path_str):
+    # create_readme = False
+    #
+    # for folder in directory_names:
+    #     for file in os.listdir(f"{directory_path}/{folder}"):
+    #         if '.json' in file:
+    #             create_readme = True
+    #             break
+    #     if create_readme:
+    #         break
+
+    # if create_readme:
+    name = directory_path.split("/")[-1]
+    print(f"Creating Folder README for {name} ({directory_path})")
+    folder_structure = pd.DataFrame(columns=['Folder', 'Files / Folders'])
+
+    for folder in directory_names:
+        files = os.listdir(f"{directory_path}/{folder}")
+        json_file_count = len([file for file in files if '.json' in file])
+        folder_count = len([file for file in files if '.json' not in file and "README.md" not in file])
+
+        if folder_count > 1:
+            folder_df = pd.DataFrame([[folder, folder_count]], columns=folder_structure.columns)
+        else:
+            folder_df = pd.DataFrame([[folder, json_file_count]], columns=folder_structure.columns)
+
+        folder_structure = pd.concat([folder_structure, folder_df])
+
+    markdown = folder_structure.to_markdown(tablefmt="github", index=False)
+
+    with open(f"{directory_path}/README.md", "w") as file:
+        file.write(markdown)
+
+# Create README files for subpages
+for directory_path, directory_names, _ in os.walk(path_str):
+    if len(directory_names) == 0:
+        if directory_path.split("/")[-1] == "Discontinued":
+            directory_path = "/".join(directory_path.split("/")[:-1])
+
+        name = directory_path.split("/")[-1]
+        all_json_name = f"_{name}.json"
+
+        print(f"Creating Series README for {name} ({directory_path})")
+
+        all_json_data = json.load(open(f"{directory_path}/{all_json_name}", "r"))
+
+        if all_json_data:
+            df = pd.DataFrame.from_dict(all_json_data, orient='index')[
+                ['id', 'title', 'observation_start', 'observation_end']]
+            markdown = df.to_markdown(tablefmt="github", index=False)
+
+            with open(f"{directory_path}/README.md", "w") as file:
+                file.write(markdown)
