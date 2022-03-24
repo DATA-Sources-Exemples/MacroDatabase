3ae692448a744e6526a61d32fdc654f72c3e7bc4
Jeroen Bouma <jer.bouma@gmail.com>
2022-03-24 10:40:37 +0100

    Add in the automatic filling of README files

--- Methodology/create_tables.py
+++ Methodology/create_tables.py
@@ -10,61 +10,8 @@ except FileNotFoundError:
     path_str = "../Database"
 
 
-# Create table overview (categories, sub-categories and files)
-table_structure = {}
-
-for top_level in os.listdir(path_str):
-    if top_level in ['.DS_Store', "README.md"]:
-        continue
-
-    table_structure[top_level] = {}
-    path_secondary = f"{path_str}/{top_level}"
-
-    for secondary_level in os.listdir(path_secondary):
-        if secondary_level in ['.DS_Store', "README.md"]:
-            continue
-
-        amount_of_files = 0
-        for directory_path, directory_names, directory_files in os.walk(f"{path_secondary}/{secondary_level}"):
-            if len(directory_names) == 0 or len(directory_names) == 1 and directory_names[0] == "Discontinued":
-                for file in directory_files:
-                    if ".json" in file:
-                        amount_of_files += 1
-
-        table_structure[top_level][secondary_level] = amount_of_files
-
-reform = {(outerKey, innerKey): values for outerKey, innerDict in table_structure.items()
-          for innerKey, values in innerDict.items()}
-df = pd.DataFrame.from_dict(reform, orient='index')
-df.index = pd.MultiIndex.from_tuples(df.index)
-
-df_print = df.reset_index()
-prev_level = [None] * df.index.nlevels
-for irow, (idx, row) in enumerate(df.iterrows()):
-    for ilevel, level in enumerate(idx):
-        if prev_level[ilevel] == level:
-            df_print.iat[irow, ilevel] = ''
-        prev_level[ilevel] = level
-
-df_print[0] = df_print[0].apply(lambda x: "{:,}".format(x).rjust(15))
-
-overview = tabulate(
-    df_print, tablefmt="github", headers=['Category', "Sub-Category", "Files"],
-    colalign=("left", "left", "right"), showindex=False)
-
-# Create README files for one layer above the folders containing json files
+# Create README files for all locations to count folders and files
 for directory_path, directory_names, _ in os.walk(path_str):
-    # create_readme = False
-    #
-    # for folder in directory_names:
-    #     for file in os.listdir(f"{directory_path}/{folder}"):
-    #         if '.json' in file:
-    #             create_readme = True
-    #             break
-    #     if create_readme:
-    #         break
-
-    # if create_readme:
     name = directory_path.split("/")[-1]
     print(f"Creating Folder README for {name} ({directory_path})")
     folder_structure = pd.DataFrame(columns=['Folder', 'Files / Folders'])
@@ -106,3 +53,48 @@ for directory_path, directory_names, _ in os.walk(path_str):
 
             with open(f"{directory_path}/README.md", "w") as file:
                 file.write(markdown)
+
+# Create table overview (categories, sub-categories and files) in the main folder
+table_structure = {}
+
+print(f"Creating Overview README in {path_str}")
+for top_level in os.listdir(path_str):
+    if top_level in ['.DS_Store', "README.md"]:
+        continue
+
+    table_structure[top_level] = {}
+    path_secondary = f"{path_str}/{top_level}"
+
+    for secondary_level in os.listdir(path_secondary):
+        if secondary_level in ['.DS_Store', "README.md"]:
+            continue
+
+        amount_of_files = 0
+        for directory_path, directory_names, directory_files in os.walk(f"{path_secondary}/{secondary_level}"):
+            if len(directory_names) == 0 or len(directory_names) == 1 and directory_names[0] == "Discontinued":
+                for file in directory_files:
+                    if ".json" in file:
+                        amount_of_files += 1
+
+        table_structure[top_level][secondary_level] = amount_of_files
+
+reform = {(outerKey, innerKey): values for outerKey, innerDict in table_structure.items()
+          for innerKey, values in innerDict.items()}
+df = pd.DataFrame.from_dict(reform, orient='index')
+df.index = pd.MultiIndex.from_tuples(df.index)
+
+df_print = df.reset_index()
+prev_level = [None] * df.index.nlevels
+for irow, (idx, row) in enumerate(df.iterrows()):
+    for ilevel, level in enumerate(idx):
+        if prev_level[ilevel] == level:
+            df_print.iat[irow, ilevel] = ''
+        prev_level[ilevel] = level
+
+df_print[0] = df_print[0].apply(lambda x: "{:,}".format(x).rjust(15))
+df_print.columns = ['Category', "Sub-Category", "Files"]
+
+markdown = df_print.to_markdown(tablefmt="github", index=False)
+
+with open(f"{path_str}/README.md", "w") as file:
+    file.write(markdown)
