fe63f8b885e263e8116aa9087276aed9c635a8d0
Jeroen Bouma <jer.bouma@gmail.com>
2022-04-04 16:53:50 +0200

    Add exception

--- Methodology/structure_series.py
+++ Methodology/structure_series.py
@@ -19,6 +19,9 @@ for directory_path, directory_names, _ in os.walk(path_str):
 
         name = directory_path.split("/")[-1]
 
+        if name in ["_Curated", "_Structure"]:
+            continue
+
         all_json_files = dict()
         all_discontinued_json_files = dict()
         all_json_files_including_discontinued = dict()
